3f822b26f0ba3540d70d938a4c8f01bb703bff83
Bluemi <bruno.schilling@protonmail.ch>
2026-03-15 14:07:32 +0100

    removed main.ts / index.html

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import p5 from "p5";
+import {Scene} from "./scene";
+import {ScenePlayer} from "./scene-player";
+
 export * from "./scene-player";
 export * from "./scene";
 export * from "./sprite";
+
+export function initScenes(sceneBuffer: [string, Scene][], audioAssets: string[], imageAssets: string[], width: number = 1920, height: number = 1080) {
+    new p5((p) => {
+        const sceneBufferMap = new Map<string, Scene>(sceneBuffer);
+        let scenePlayer = new ScenePlayer(p, sceneBufferMap, sceneBuffer[0][0]);
+
+        p.setup = () => {
+            p.createCanvas(width, height).parent('sketch-holder');
+            scenePlayer.load(audioAssets, imageAssets).then(() => {});
+        };
+
+        p.draw = () => {
+            scenePlayer.update();
+        };
+    });
+}
