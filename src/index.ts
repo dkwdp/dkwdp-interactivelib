a5420572fbbcc4b24e400a24843ecba0e139ca9f
Bluemi <bruno.schilling@protonmail.ch>
2026-03-23 11:22:18 +0100

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,11 @@ export * from "./context";
 
 export function initScenes(parent: HTMLElement, sceneBuffer: [string, Scene][], audioAssets: string[], imageAssets: string[]) {
     new p5((p) => {
-        const sceneBufferMap = new Map<string, Scene>(sceneBuffer);
         let scenePlayer: ScenePlayer;
 
         p.setup = () => {
             p.createCanvas().parent(parent);
-            scenePlayer = new ScenePlayer(p, sceneBufferMap, sceneBuffer[0][0]);
+            scenePlayer = new ScenePlayer(p, new Map<string, Scene>(sceneBuffer), sceneBuffer[0][0]);
             scenePlayer.load(audioAssets, imageAssets).then(() => {});
         };
 
