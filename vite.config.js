43d5c445f02b9664c2637230a1e57343b4af1625
Bluemi <bruno.schilling@protonmail.ch>
2026-03-30 17:15:51 +0200

    rename package -> dkwdpil

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'DkwdpInteractive',
-      fileName: 'dkwdp-interactivelib',
+      fileName: 'dkwdpil',
       formats: ['es', 'umd']
     },
     rollupOptions: {
